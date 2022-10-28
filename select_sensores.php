ec29d568880d55f9d08ce3ee57ad7389bf656fbb
Alysson-Alves23 <alyssonja23@gmail.com>
2022-10-28 18:40:19 -0300

    fix db

--- select_sensores.php
+++ select_sensores.php
@@ -7,7 +7,7 @@ $MYSQL_ADDON_PORT="3306";
 $MYSQL_ADDON_URI="mysql://us8beikwegk8lha0:ibBbuZFJrZAA7GyR5L44@b633vz1opiwq7syycoru-mysql.services.clever-cloud.com:3306/b633vz1opiwq7syycoru";
 $MYSQL_ADDON_USER="us8beikwegk8lha0";
 $MYSQL_ADDON_VERSION="8.0";
-$conexao = mysqli_connect($MYSQL_ADDON_HOST,$MYSQL_ADDON_USER,$MYSQL_ADDON_PASSWORD,$MYSQL_ADDON_DB);
+$conexao = mysql_connect($MYSQL_ADDON_HOST,$MYSQL_ADDON_USER,$MYSQL_ADDON_PASSWORD,$MYSQL_ADDON_DB);
 
 if($conexao->error){
 	die("Error: " . mysqli.error);
@@ -16,11 +16,7 @@ if($conexao->error){
 
 
 $query = "SELECT `Temperatura`,`Evapotranspiração`,`Data`,`Hora` FROM b633vz1opiwq7syycoru.dados WHERE data = '2022-10-05'";  
-$result = mysqli_query($conexao,$query);
+$result = mysql_query($conexao,$query);
 $records = array();
-while( $rows = mysqli_fetch_assoc($result) ) {
-	$records[] = $rows;
-}
- $result->fetch(\PDD::FETCH_ASSOC);
-
+    echo mysql_resul($result, 2);
 ?>
